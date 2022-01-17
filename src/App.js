ef62a6c8f9264f3af9b8b23f2c700c339de7e726
gabrielbadarau <badaraugabriel95@gmail.com>
2022-01-17 19:34:47 +0200

    installed react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Routes, Route} from "react-router-dom";
+import HomePage from './pages/Home/HomePage'
+import ErrorPage from './pages/Page404/ErrorPage'
 import './App.css';
 
 function App() {
   return (
-    <div>
-      
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Page404/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Page404() {
+    return (
+        <div className="container-fluid container-min-max-width mt-3 w-25">
+            <h1>404 ERROR</h1>
+        </div>
+    )
+}
+
+export default Page404;
