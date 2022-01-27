11ce2720846c3bacfa801ea0a2018fe8deb3f24d
gabrielbadarau <badaraugabriel95@gmail.com>
2022-01-27 12:38:10 +0200

    transform to function comp using setState hook

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MovieList from '../../components/MovieList'
 import axios from 'axios';
 import themoviedbApiKey from '../../configs/themoviedb';
@@ -7,73 +7,67 @@ import Layout from '../../components/Layout';
 import { ReactComponent as Search } from '../../assets/icons/search.svg';
 import ScrollButton from '../../components/ScrollButton'
 
-class HomePage extends React.Component{
+function HomePage(){
 
-    constructor(props){
-        super(props);
-        this.state={
-            moviesList:[],
-            checkEmptyList:false
-        };
-    }
-
-    fetchData(){
-        let param=document.getElementById('search').value.split(' ').join('%20')
-        let url=`https://api.themoviedb.org/3/search/movie?api_key=${themoviedbApiKey}&language=en-US&query=${param}&include_adult=false`;
-
-        axios.request(url)
-        .then(response=>{
-            const moviesList=response.data.results;
-            if(moviesList.length){
-                this.setState({moviesList,checkEmptyList:false})
-            }
-            else(
-                this.setState({moviesList,checkEmptyList:true})
-            )
-        })
-        .catch(function(error){
-            console.log(error)
-        })
-    }
-
-    submitSearch(event){
+    const [state,setState]=useState({
+        moviesList:[],
+        checkEmptyList:false
+    })
+    
+    function submitSearch(event){
         event.preventDefault();
-        this.fetchData()
+        fetchData()
         document.getElementById('search').value=""
     }
 
-    render(){
-        console.log(this.state.moviesList)
-        return (
-            <Layout>
-                <div className="container-fluid container-min-max-width mt-3">
-                    <div className="text-center "> 
-                        <form
-                        className='searchForm'
-                        onSubmit={(event)=>this.submitSearch(event)}
-                        >
-                            <label htmlFor="search"></label>
-                            <input
-                                type="text"
-                                id="search"
-                                placeholder='Search for a movie...'
-                            />
-                            <label>
-                                <input
-                                    id="submitButton"
-                                    type="submit" 
-                                />
-                                <Search style={{marginLeft:"2px",marginBottom:"5px"}} className='search-svg' />
-                            </label>
-                        </form>
-                    </div>
-                    <MovieList list={this.state.moviesList} />
-                    {this.state.checkEmptyList===true ? <h1>Nothing found</h1>  : null}
-                </div>
-                <ScrollButton />
-            </Layout>
-        )
+    function fetchData(){
+        let param=document.getElementById('search').value.split(' ').join('%20')
+            let url=`https://api.themoviedb.org/3/search/movie?api_key=${themoviedbApiKey}&language=en-US&query=${param}&include_adult=false`;
+    
+            axios.request(url)
+            .then(response=>{
+                const moviesList=response.data.results;
+                if(moviesList.length){
+                    setState({moviesList,checkEmptyList:false})
+                }
+                else(
+                    setState({moviesList,checkEmptyList:true})
+                )
+            })
+            .catch(function(error){
+                console.log(error)
+            })
     }
+
+    return (
+        <Layout>
+            <div className="container-fluid container-min-max-width mt-3">
+                <div className="text-center "> 
+                    <form
+                    className='searchForm'
+                    onSubmit={(event)=>submitSearch(event)}
+                    >
+                        <label htmlFor="search"></label>
+                        <input
+                            type="text"
+                            id="search"
+                            placeholder='Search for a movie...'
+                        />
+                        <label>
+                            <input
+                                id="submitButton"
+                                type="submit" 
+                            />
+                            <Search style={{marginLeft:"2px",marginBottom:"5px"}} className='search-svg' />
+                        </label>
+                    </form>
+                </div>
+                <MovieList list={state.moviesList} />
+                {state.checkEmptyList===true ? <h1>Nothing found</h1>  : null}
+            </div>
+            <ScrollButton />
+        </Layout>
+    )
 }
 
 export default HomePage
