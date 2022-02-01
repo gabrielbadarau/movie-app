67970ee4736a0efc361b05fbb18c05e76e302185
gabrielbadarau <badaraugabriel95@gmail.com>
2022-02-01 20:45:54 +0200

    changed the css of the header navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,34 +9,36 @@ import {logout} from '../redux/actions/signInMethod';
 function Header(props){
 
     return(
-        <header className='border-bottom header-height container-fluid d-flex justify-content-around align-items-center text-center'>
-            {/* <div className='header-height container-fluid container-min-max-width d-flex justify-content-between align-items-center text-center'> */}
-                <div className=''>
-                    <Link to='/'>
-                        <img src={Logo} alt="logo" className=""/>
-                    </Link>
-                </div>
-                <div className=''>
+        <header className='border-bottom header-height container-fluid d-flex justify-content-between align-items-center text-center'>
+            <div className=''>
+                <Link to='/'>
+                    <img src={Logo} alt="logo" className=""/>
+                </Link>
+            </div>
+            <div className='d-flex flex-row flex-nowrap align-items-center'>
+                <div className='m-3'>
                     <Link className='header-text' to='/my-watch-list'>
                         Watch List
                     </Link>
                 </div>
-                <div className=''>
+                <div className='m-3'>
                     <Link className='header-text' to='/my-favorite-list'>
                         Favorite List
                     </Link>
                 </div>
-                {props.user ?
-                    <div className='d-flex flex-row align-items-center'>
-                        <p style={{cursor:'default'}} className='login-name header-text mt-2'>{props.user}</p>
-                        <Logout 
-                            className='logoutIcon'
-                            onClick={()=>props.logout()}
-                        />
-                    </div>
-                    : <Link className='header-text' to="/login">Login</Link>
-                }
-            {/* </div> */}
+                <div className='m-3'>
+                    {props.user ?
+                        <div className='d-flex flex-row align-items-center'>
+                            <p style={{cursor:'default'}} className='mt-3 login-name header-text '>{props.user}</p>
+                            <Logout 
+                                className='logoutIcon'
+                                onClick={()=>props.logout()}
+                            />
+                        </div>
+                        : <Link className='header-text' to="/login">Login</Link>
+                    }
+                </div>
+            </div>
         </header>
     )
 
